2f22d1afb564fa6e11fdf9316c669ea977d71888
dsh <d.henty@epcc.ed.ac.uk>
2021-07-23 16:48:10 +0100

    Updated .

--- P-SER/jacobi_numpy.py
+++ P-SER/jacobi_numpy.py
@@ -4,6 +4,7 @@
 import numpy as np
 
 import sys
+import math
 
 def jacobi(niter, psi):
 
@@ -11,13 +12,26 @@ def jacobi(niter, psi):
     m = m - 2
     n = n - 2
 
-    tmp = np.zeros((m+2, n+2))
+    bnorm = math.sqrt(np.sum(psi[:][:]*psi[:][:]))
+
+    psitmp = np.zeros((m+2, n+2))
+
     for iter in range(1,niter+1):
+
         # Use index notation and offsets to compute the stream function
-        tmp[1:m+1,1:n+1] = 0.25 * (psi[2:m+2,1:n+1]+psi[0:m,1:n+1]+psi[1:m+1,2:n+2]+psi[1:m+1,0:n])
+        psitmp[1:m+1,1:n+1] = 0.25 * (psi[2:m+2,1:n+1]+psi[0:m,1:n+1]+psi[1:m+1,2:n+2]+psi[1:m+1,0:n])
+
+        if (iter == niter):
+
+            error = np.sum(   (psitmp[1:m+1,1:n+1]-psi[1:m+1,1:n+1])*(psitmp[1:m+1,1:n+1]-psi[1:m+1,1:n+1]) )
+
+            error = math.sqrt(error)
+            error = error/bnorm
+
+            sys.stdout.write("Final error is {0}\n".format(error))
 
         # Update psi
-        np.copyto(psi[1:m+1,1:n+1], tmp[1:m+1,1:n+1])
+        np.copyto(psi[1:m+1,1:n+1], psitmp[1:m+1,1:n+1])
 
         if iter%1000 == 0:
             sys.stdout.write("completed iteration {0}\n".format(iter))
