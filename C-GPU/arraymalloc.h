bc52a84726ac6a76fdb0e25b99b4bbc99191ccf3
David Henty <d.henty@epcc.ed.ac.uk>
2024-06-04 16:06:36 +0100

    Updated C-GPU C-MPI C-OMP C-SER/

--- C-GPU/arraymalloc.h
+++ C-GPU/arraymalloc.h
@@ -1,4 +1,2 @@
-#include <stddef.h>
-
-void **arraymalloc2d(int nx, int ny, size_t typesize);
-
+void  **arraymalloc2d(int nx, int ny,         size_t typesize);
+void ***arraymalloc3d(int nx, int ny, int nz, size_t typesize);
--- C-MPI/arraymalloc.c
+++ C-MPI/arraymalloc.c
@@ -1,29 +1,60 @@
-#include "arraymalloc.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 void **arraymalloc2d(int nx, int ny, size_t typesize)
 {
-  int i;
+  size_t it, nxt, nyt, mallocsize;
   void **array2d;
 
-  size_t mallocsize;
+  nxt = nx;
+  nyt = ny;
 
   // total memory requirements including pointers
 
-  mallocsize = nx*sizeof(void *) + nx*ny*typesize;
+  mallocsize = nxt*sizeof(void *) + nxt*nyt*typesize;
 
   array2d = (void **) malloc(mallocsize);
 
   // set first pointer to first element of data
 
-  array2d[0] = (void *) (array2d + nx);
+  array2d[0] = (void *) (array2d + nxt);
 
-  for(i=1; i < nx; i++)
+  for(it=1; it < nxt; it++)
     {
       // set other pointers to point at subsequent rows
 
-      array2d[i] = (void *) (((char *) array2d[i-1]) + ny*typesize);
+      array2d[it] = (void *) (((char *) array2d[it-1]) + nyt*typesize);
     }
 
   return array2d;
+}
+
+void ***arraymalloc3d(int nx, int ny, int nz, size_t typesize)
+{
+  void ***array3d;
+
+  size_t it, jt, nxt, nyt, nzt, mallocsize;
+
+  nxt = nx;
+  nyt = ny;
+  nzt = nz;
+
+  // total memory requirements including pointers
+
+  mallocsize = (nxt + nxt*nyt)*sizeof(void *) + nxt*nyt*nzt*typesize;
+
+  array3d = (void ***) malloc(mallocsize);
+
+  for (it=0; it < nxt; it++)
+    {
+      array3d[it] = (void *) (array3d + nxt + nyt*it);
+
+      for (jt=0; jt < nyt; jt++)
+      {
+        array3d[it][jt] = (void *) ( ((char *) (array3d + nxt + nxt*nyt)) + (nyt*nzt*it + nzt*jt)*typesize);
+        
+      }
+    }
+  return array3d;
 }
